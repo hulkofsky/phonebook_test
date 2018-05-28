0bbccad4c98693a3cc5fb0ff0fa5ea0240583971
Slon <hulkofsky@gmail.com>
2018-05-28 20:35:08 +0300

    contactlist done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import {connect} from 'react-redux'
+import { Grid, Header, Image, List } from 'semantic-ui-react'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Grid>
+        <Grid.Column width={6}>
+          <Header size='large'>Contacts:</Header>
+          <List animated celled verticalAlign='middle'>
+            {this.props.contacts.map((contact, index) => 
+              <List.Item key={index}>
+                <Image avatar src={contact.photo}/>
+                <List.Content>
+                    <List.Header>{contact.name}</List.Header>
+                    {contact.company}
+                </List.Content>
+              </List.Item>
+            )}
+          </List>
+          </Grid.Column>
+      </Grid>
     );
   }
 }
 
-export default App;
+export default connect(
+  state => ({
+    contacts: state.contacts
+  }),
+  dispatch => ({})
+)(App);
