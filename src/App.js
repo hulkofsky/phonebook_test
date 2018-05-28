9dfa7d192eff409b974905e0c1000dd12a9f5787
Slon <hulkofsky@gmail.com>
2018-05-28 20:53:05 +0300

    search by contact name done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
-import { Grid, Header, Image, List } from 'semantic-ui-react'
+import { Grid, Header, Image, List, Input } from 'semantic-ui-react'
 import './App.css';
 
 class App extends Component {
+  
+  findContact() {
+    const searchCriteria = document.querySelector('input[name=search]').value;
+    this.props.onFindContact(searchCriteria);
+  }
+  
   render() {
     return (
       <Grid>
         <Grid.Column width={6}>
+          <Input name='search' icon='search' iconPosition='left' placeholder='Search users...'
+            onChange={this.findContact.bind(this)} 
+          />
           <Header size='large'>Contacts:</Header>
           <List animated celled verticalAlign='middle'>
             {this.props.contacts.map((contact, index) => 
@@ -28,7 +37,11 @@ class App extends Component {
 
 export default connect(
   state => ({
-    contacts: state.contacts
+    contacts: state.contacts.filter(contact =>  contact.name.toLowerCase().includes(state.filterContacts.toLowerCase()))
   }),
-  dispatch => ({})
+  dispatch => ({
+    onFindContact: (searchCriteria) => {
+      dispatch({ type: 'FIND_CONTACT', payload: searchCriteria })
+    },
+  })
 )(App);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import contacts from './contacts'
+import filterContacts from './filterContacts'
 
 export default combineReducers({
-    contacts
+    contacts,
+    filterContacts
 })
